d6951988e81657a0d983cecd09e4e322ac02f456
Nguyen Ton Hung <hungnt@hoangkhang.com.vn>
2016-04-07 20:16:08 +0700

    update layout frontend-v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   
   protected
     def layout_by_resource
-      if controller_name == "home"
+      if controller_name == "home" || controller_name == "account" || controller_name == "checkout" || controller_name == "blog" ||
+        controller_name == "information" || controller_name == "manufacturer" || controller_name == "product"
         "frontend"
       elsif controller_name == "main"
         "backend"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,38 @@
 Rails.application.routes.draw do
   root "home#index"
   
+  get "account/login" => "account#login", as: :login
+  get "account/forgotten" => "account#forgotten", as: :forgotten
+  get "account/my_account" => "account#my_account", as: :my_account
+  get "account/wishlist" => "account#wishlist", as: :wishlist
+  get "account/register" => "account#register", as: :register
+  get "account/order" => "account#order", as: :order
+  get "account/address" => "account#address", as: :address
+  
+  get "checkout/cart" => "checkout#cart", as: :cart
+  get "checkout/checkout" => "checkout#checkout", as: :checkout
+  
+  get "blog" => "blog#index", as: :index
+  get "blog/show" => "blog#show", as: :show
+  
+  get "information/about_us" => "information#about_us", as: :about_us
+  get "information/contact_us" => "information#contact_us", as: :contact_us
+  get "information/delivery" => "information#delivery", as: :delivery
+  get "information/faq" => "information#faq", as: :faq
+  get "information/privacy_policy" => "information#privacy_policy", as: :privacy_policy
+  get "information/terms_conditions" => "information#terms_conditions", as: :terms_conditions
+  get "information/site_map" => "information#site_map", as: :site_map
+  
+  get "manufacturer/list" => "manufacturer#list", as: :list
+  get "manufaturer/products" => "manufaturer#products", as: :products
+  
+  get "product/category" => "product#category", as: :category
+  get "product/comparison" => "product#comparison", as: :comparison
+  get "product/product" => "product#product", as: :product
+  get "product/quickview" => "product#quickview", as: :quickview
+  get "search/products" => "product#search", as: :search
+  get "product/testimonial" => "product#testimonial", as: :testimonial
+  
   namespace :admin, path: "shopme-admin" do
     get "main" => "main#index"
   end
