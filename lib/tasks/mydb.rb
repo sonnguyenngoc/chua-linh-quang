b1ebd979f3c1a20e6738a8769fcbbb508947c0d5
Luan Pham <luanpm88@gmail.com>
2016-04-13 18:33:39 -0700

    add task mydb truncate: update 2

--- lib/tasks/mydb.rb
+++
@@ -1,9 +0,0 @@
-namespace :mydb do
-  desc "Truncate all tables"
-  task :truncate => :environment do
-    conn = ActiveRecord::Base.connection
-    tables = conn.execute("show tables").map { |r| r[0] }
-    tables.delete "schema_migrations"
-    tables.each { |t| conn.execute("TRUNCATE #{t}") }
-  end
-end
