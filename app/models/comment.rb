de22b88165580ba40a77344ed50803e67e17b3df
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-04-28 09:28:49 +0700

    update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates :name, :content, :star, presence: true
   belongs_to :product
   belongs_to :user
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
+  validates :name, :email, :content, presence: true
   belongs_to :product
 end
