c3cf151e150fb0ee2cdbe13faac2efd90b5130b3
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-05-13 09:38:31 +0700

    update

--- app/controllers/comment_articles_controller.rb
+++ app/controllers/comment_articles_controller.rb
@@ -20,19 +20,27 @@ class CommentArticlesController < ApplicationController
   # GET /comment_articles/1/edit
   def edit
   end
+  
+  def verify_google_recaptcha(secret_key,response)
+    status = `curl "https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{response}"`
+    logger.info "---------------status ==> #{status}"
+    hash = JSON.parse(status)
+    hash["success"] == true ? true : false
+  end
 
   # POST /comment_articles
   # POST /comment_articles.json
   def create
+    @secret_key = "6Le7mh8TAAAAAGKRPjxYnO9t0O1_m8dgxa-EgcOB"
     @comment_article = CommentArticle.new(comment_article_params)
-
+  
+    status = verify_google_recaptcha(@secret_key, params["g-recaptcha-response"])
     respond_to do |format|
-      if @comment_article.save
+      if @comment_article.save && status
         format.html { redirect_to controller: "blog", action: "show", blog_id: @comment_article.article_id }
-        format.json { render :show, status: :created, location: @comment_article }
       else
-        format.html { render :new }
-        format.json { render json: @comment_article.errors, status: :unprocessable_entity }
+        flash[:notice] = "Đăng bình luận thất bại"
+        format.html { redirect_to controller: "blog", action: "show", blog_id: @comment_article.article_id }
       end
     end
   end
