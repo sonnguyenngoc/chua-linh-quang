147c9af950103157457449c9d95c2062fb654b0f
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-05-17 09:39:08 +0700

    login with google +

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.find_for_facebook(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
@@ -13,7 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
   
   def google_oauth2
-     
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
  
     if @user.persisted?
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,29 @@ class User < ActiveRecord::Base
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :omniauthable, :validatable,
            :omniauthable
-           
-    def self.from_omniauth(auth)
-        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-            user.provider = auth.provider
-            user.uid = auth.uid
-            user.email = auth.info.email
-            user.first_name = auth.info.first_name
-            user.last_name = auth.info.last_name
-            user.oauth_token = auth.credentials.token
-            user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-            user.password = Devise.friendly_token[0,20]
-        end
+    
+    def self.find_for_facebook(access_token, signed_in_resource=nil)
+        data = access_token.info
+        user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+        if user
+            return user
+        else
+            registered_user = User.where(:email => access_token.info.email).first
+            if registered_user
+                return registered_user
+            else
+                user = User.create(
+                    first_name: data["first_name"],
+                    last_name: data["last_name"],
+                    provider:access_token.provider,
+                    email: data["email"],
+                    uid: access_token.uid,
+                    oauth_token: access_token.credentials.token,
+                    oauth_expires_at: Time.at(access_token.credentials.expires_at),
+                    password: Devise.friendly_token[0,20],
+                )
+            end
+       end
     end
     
     def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
