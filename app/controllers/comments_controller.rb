55844fd6a9bac5e6461f30d97f3a6d2f76476dc2
Nguyen Ton Hung <hungnt@hoangkhang.com.vn>
2016-05-17 14:55:23 +0700

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -68,6 +68,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:product_id, :name, :content, :star)
+      params.require(:comment).permit(:product_id, :name, :content, :star, :user_id)
     end
 end
