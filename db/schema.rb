3a69b084b06be12a85f1b18d5ad40a5e58f217e6
Luan Pham <luanpm88@gmail.com>
2016-05-20 18:51:45 -0700

    fix chat bug

--- db/schema.rb
+++ db/schema.rb
@@ -250,9 +250,9 @@ ActiveRecord::Schema.define(version: 20160520062835) do
 
   create_table "newsletters", force: :cascade do |t|
     t.string   "email"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.boolean  "status",     default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean  "status"
   end
 
   create_table "options", force: :cascade do |t|
