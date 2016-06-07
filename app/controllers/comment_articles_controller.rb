dd543e1d94b8f9a1c70a39d69c822c3683916031
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-06-06 23:22:17 -0700

    update

--- app/controllers/comment_articles_controller.rb
+++ app/controllers/comment_articles_controller.rb
@@ -1,64 +1,28 @@
 class CommentArticlesController < ApplicationController
-  before_action :set_comment_article, only: [:show, :edit, :update, :destroy]
-
-  # GET /comment_articles
-  # GET /comment_articles.json
-  def index
-    @comment_articles = CommentArticle.all
-  end
-
-  # GET /comment_articles/1
-  # GET /comment_articles/1.json
-  def show
-  end
-
-  # GET /comment_articles/new
-  def new
-    @comment_article = CommentArticle.new
-  end
-
-  # GET /comment_articles/1/edit
-  def edit
+  def verify_google_recaptcha(secret_key,response)
+    status = `curl "https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{response}"`
+    logger.info "---------------status ==> #{status}"
+    hash = JSON.parse(status)
+    hash["success"] == true ? true : false
   end
 
   # POST /comment_articles
   # POST /comment_articles.json
   def create
     @comment_article = CommentArticle.new(comment_article_params)
+    @secret_key = "6Le7mh8TAAAAAGKRPjxYnO9t0O1_m8dgxa-EgcOB"
     @comment_article.user_id = current_user.id if current_user.present?
     @comment_article.parent_id = params[:parent_id] if params[:parent_id].present?
-  
+    
+    status = verify_google_recaptcha(@secret_key, params["g-recaptcha-response"])
     respond_to do |format|
-      if @comment_article.save
+      if status
+        @comment_article
         format.html { redirect_to controller: "blog", action: "show", blog_id: @comment_article.article_id }
       else
         flash[:notice] = "Đăng bình luận không thành công"
         format.html { redirect_to controller: "blog", action: "show", blog_id: @comment_article.article_id }
       end
-    end
-  end
-
-  # PATCH/PUT /comment_articles/1
-  # PATCH/PUT /comment_articles/1.json
-  def update
-    respond_to do |format|
-      if @comment_article.update(comment_article_params)
-        format.html { redirect_to @comment_article, notice: 'Comment article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment_article }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment_article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comment_articles/1
-  # DELETE /comment_articles/1.json
-  def destroy
-    @comment_article.destroy
-    respond_to do |format|
-      format.html { redirect_to comment_articles_url, notice: 'Comment article was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
