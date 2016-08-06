e80cef96695dc3f65db86329bbd1e250cd9408b5
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-08-06 06:34:29 -0700

    update

--- app/controllers/cultural_controller.rb
+++ app/controllers/cultural_controller.rb
@@ -1,18 +1,18 @@
 class CulturalController < ApplicationController
     def index
-        @articles = Article.get_all_cultural_blogs.paginate(:page => params[:page], :per_page => 3)
+        @articles = Article.get_all_cultural_blogs.paginate(:page => params[:page], :per_page => 9)
     end
     
     def cultural_sub_1
-        @articles = Article.get_all_cultural_sub_1_blogs.paginate(:page => params[:page], :per_page => 3)
+        @articles = Article.get_all_cultural_sub_1_blogs.paginate(:page => params[:page], :per_page => 8)
     end
     
     def cultural_sub_2
-        @articles = Article.get_all_cultural_sub_2_blogs.paginate(:page => params[:page], :per_page => 3)
+        @articles = Article.get_all_cultural_sub_2_blogs.paginate(:page => params[:page], :per_page => 8)
     end
     
     def cultural_sub_3
-        @galleries = Gallery.get_all_galleries.paginate(:page => params[:page], :per_page => 3)
+        @galleries = Gallery.get_all_galleries.paginate(:page => params[:page], :per_page => 8)
     end
     
     def cultural_sub_4
