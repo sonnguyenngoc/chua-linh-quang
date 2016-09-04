0617494727660628c0cd131c5e0e381f8d653a17
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-09-03 18:17:10 -0700

    update

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,14 +1,14 @@
 class CategoryController < ApplicationController
   def index
     @category = ArticleCategory.find(params[:category_id])
-    @posts = @category.get_posts_for_categories(params).paginate(:page => params[:page], :per_page => 12)
+    @posts = @category.get_posts_for_categories(params).paginate(:page => params[:page], :per_page => 14)
   end
   
   def hot_news
-    @posts = Article.get_all_hot_news.paginate(:page => params[:page], :per_page => 12)
+    @posts = Article.get_all_hot_news.paginate(:page => params[:page], :per_page => 14)
   end
   
   def posts_search
-    @posts = Article.search(params).paginate(:page => params[:page], :per_page => 12)
+    @posts = Article.search(params).paginate(:page => params[:page], :per_page => 14)
   end
 end
--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,14 +1,14 @@
 class GalleryController < ApplicationController
   def music
-    @musics = Album.get_all_musics.paginate(:page => params[:page], :per_page => 12)
+    @musics = Album.get_all_musics.paginate(:page => params[:page], :per_page => 14)
   end
 
   def picture
-    @galleries = Gallery.get_all_galleries.paginate(:page => params[:page], :per_page => 12)
+    @galleries = Gallery.get_all_galleries.paginate(:page => params[:page], :per_page => 14)
   end
 
   def video
-    @videos = Album.get_all_videos.paginate(:page => params[:page], :per_page => 12)
+    @videos = Album.get_all_videos.paginate(:page => params[:page], :per_page => 14)
   end
   
   def picture_detail
