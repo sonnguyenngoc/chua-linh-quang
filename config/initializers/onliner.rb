90f9a1d429c15f40d116213800a92a2b0a1651ab
Son Nguyen Ngoc <sonnn@hoangkhang.com.vn>
2017-01-23 14:05:09 +0700

    update from server

--- config/initializers/onliner.rb
+++ config/initializers/onliner.rb
@@ -3,7 +3,7 @@ require "redis"
 if ( Rails.env.development? || Rails.env.test? )
 	uri = URI.parse("localhost:6379")
 else
-	uri = URI.parse(ENV["REDISTOGO_URL"])
+	uri = URI.parse("localhost:6379")
 end
 
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
